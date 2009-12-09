eef2591ac5a46dbe8aa0cc08d9dbbe4169e6d36c
yono <yono.05@gmail.com>
2009-12-09 18:43:35 +0900

    setup.py を作成

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup( name = 'markovchains',
+       version = '0.0.1',
+       scripts = ['bin/mc_learn','bin/mc_talk'],
+       py_modules=['markovchains'],
+     )
